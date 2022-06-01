df8a62e2f0cf15b7bcfde403768097548876660e
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-01 14:55:27 +0200

    add react

---
+++ projects/react_native_sample/MyReactNative/App.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getPeople = async () => {
+     try {
+//      const response = await fetch('https://reactnative.dev/movies.json');
+      const response = await fetch('http://h2908727.stratoserver.net:8080/people');
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getPeople();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.firstname} {item.lastname}</Text>
+          )}
+        />
+      )}
+    </View>
+  );
+};
